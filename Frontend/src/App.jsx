9e1de53d2f79d4a52e21d1f5550a2de9a3243ee3
ygstudio-game <yadnyeshsunilborole@gmail.com>
2026-01-13 22:24:50 +0530

    updated modules

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import IndexPage from "./index.jsx";
 import CodeArena from "./Components/EventSection/CodeArena.jsx";
-import "./app.css"
 import CodeArena4 from "./Components/CodeArena4/CodeArena4.jsx";
 const App = () => {
   return (
